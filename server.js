a6939a1b53f24734e20b5509c501daccf97b48f3
Yusuf ÇOBAN <pyusuf24@gmail.com>
2023-05-23 13:53:14 +0300

    gitignore

--- server.js
+++ server.js
@@ -5,21 +5,22 @@ const app = require('./app');
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
+	'<PASSWORD>',
+	process.env.DATABASE_PASSWORD
 );
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log('DB connection successful!');
-  });
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log('DB connection successful!');
+	});
 
 const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+	console.log(`App running on port ${port}...`);
 });
+//deneme
