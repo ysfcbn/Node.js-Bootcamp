fba47a6a09f193eb4faf3e7d6f4d01d4bcd45dec
Yusuf ÇOBAN <pyusuf24@gmail.com>
2023-05-29 20:34:24 +0300

    new description

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -85,9 +85,17 @@ exports.updateTour = async (req, res) => {
 	}
 };
 
-exports.deleteTour = (req, res) => {
-	res.status(204).json({
-		status: 'OK',
-		data: null,
-	});
+exports.deleteTour = async (req, res) => {
+	try {
+		const tour = await Tour.findByIdAndDelete(req.params.id);
+		res.status(204).json({
+			status: 'success',
+			data: null,
+		});
+	} catch (error) {
+		res.status(404).json({
+			status: 'fail',
+			message: error,
+		});
+	}
 };
